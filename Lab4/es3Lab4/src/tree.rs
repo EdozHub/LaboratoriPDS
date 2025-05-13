60362cd590ff5796868d2513a7696d53b3a5df71
EdozHub <edoard01@yahoo.it>
2025-05-13 16:55:10 +0200

    test superati per build e add

--- Lab4/es3Lab4/src/tree.rs
+++ Lab4/es3Lab4/src/tree.rs
@@ -9,14 +9,14 @@ struct Tree{
 impl Tree{
     fn new() -> Self{
         Tree{
-            children: HashMap::new(),
-            father: HashMap::new(),
-            switches: HashMap::new()
+            children: HashMap::from([("root".to_string(), Vec::new())]),
+            father: HashMap::from([("root".to_string(), "root".to_string())]),
+            switches: HashMap::from([("root".to_string(), false)]),
         }
     }
-    fn add(mut self, father: &str, node: &str) {
+    fn add(&mut self, father: &str, node: &str) {
         self.children
-            .entry(father.to_string())
+            .entry(father.to_string().clone())
             .or_insert_with(Vec::new)
             .push(node.to_string());
         self.father
@@ -27,7 +27,36 @@ impl Tree{
             .insert(node.to_string(), false);
     }
 
-    fn remove(&self: Tree, node: &str)->Self {
+    fn find_relatives_recursive(nodes: Vec<String>, father: &str, found: &bool) -> Vec<String>{
         unimplemented!()
+    }
+
+    fn remove(&self: Tree, node: &str){
+        let mut nodes: Vec<String>=Vec::new();
+        nodes.push(node.to_string());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Tree;
+    fn build_tree() -> Tree{
+        let mut tree = Tree::new();
+        tree.add("root", "A");
+        tree.add("A", "B");
+        tree.add("A", "C");
+        tree.add("B", "D");
+        tree.add("B", "E");
+        tree.add("C", "F");
+        tree.add("C", "G");
+        tree
+    }
+    #[test]
+    fn test_build_tree() {
+        let tree = build_tree();
+        assert_eq!(tree.children.get("root").unwrap(), &vec!["A"]);
+        assert_eq!(tree.children.get("A").unwrap(), &vec!["B", "C"]);
+        assert_eq!(tree.children.get("B").unwrap(), &vec!["D", "E"]);
+        assert_eq!(tree.children.get("C").unwrap(), &vec!["F", "G"]);
     }
 }
---
+++ Lab4/es3Lab4/target/debug/deps/es3Lab4-0111f66fe5fbf373.d
@@ -0,0 +1,6 @@
+C:\Users\edoar\Desktop\Programmi\LaboratoriPDS\Lab4\es3Lab4\target\debug\deps\es3Lab4-0111f66fe5fbf373.exe: src\main.rs src\tree.rs
+
+C:\Users\edoar\Desktop\Programmi\LaboratoriPDS\Lab4\es3Lab4\target\debug\deps\es3Lab4-0111f66fe5fbf373.d: src\main.rs src\tree.rs
+
+src\main.rs:
+src\tree.rs:
